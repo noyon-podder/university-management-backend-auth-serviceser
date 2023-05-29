0f3a77e5f86ad8ffe4b2347ee2f6651ac8d34490
noyon-podder <noyon.vai369@gmail.com>
2023-05-29 13:44:59 +0600

    [Done] setup express, mongoose, cors, parser

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+//use cors
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("Connected to mongo 🧠");
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.error(`Failed to connect to mongo`, err);
+  }
+}
+
+main();
