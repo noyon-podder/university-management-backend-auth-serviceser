a685d597f1be05c1456098303648dac95f71d554
noyon-podder <noyon.vai369@gmail.com>
2023-06-04 10:48:46 +0600

    [DONE] dynamic id generate successfully

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
-
+import userRoute from '../src/app/modules/users/users.route'
+import userServices from './app/modules/users/user.services'
 const app: Application = express()
 
 //use cors
@@ -10,7 +11,14 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded())
 
-app.get('/', (req: Request, res: Response) => {
+app.use('/api/v1/users', userRoute)
+
+app.get('/', async (req: Request, res: Response) => {
+  await userServices.createUser({
+    id: '09090',
+    password: '234234',
+    role: 'faculty',
+  })
   res.send('Hello World!')
 })
 
